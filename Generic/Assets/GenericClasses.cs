9dd3f9d9c7c380a381dd35156472968b769a9aa2
Fethi Isfarca <fisfarca@googlemail.com>
2019-02-07 21:37:53 +0100

    Adding project.

---
+++ Generic/Assets/GenericClasses.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class GenericClasses : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        // Declare variables
+        GenericClass<string> instanceString = new GenericClass<string>();
+
+        instanceString.Print("Hello World!");
+    }
+}
+
+public class GenericClass<T>
+{
+    // Output the data
+    public void Print(T data)
+    {
+        Debug.LogFormat("Type: {0} | Value: {1}", typeof(T).ToString(), data);
+    }
+}
